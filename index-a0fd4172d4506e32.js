9b84d5d2e02bdfbaf20f457c5191a063dc0e2619
AnhTuDo1998 <AnhTuDo1998@users.noreply.github.com>
2022-03-02 13:22:56 +0000

    deploy: 0ebe1fe4c0bcfb0b1079d99b34b9010112b57e29

--- index-a0fd4172d4506e32.js
+++ index-a0fd4172d4506e32.js
@@ -298,7 +298,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-716b70c94c61954f_bg.wasm', import.meta.url);
+        input = new URL('index-a0fd4172d4506e32_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
